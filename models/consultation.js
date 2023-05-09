849be5cb0da7133160c2aa657add072665d5bd4f
ambrurek <109169637+ambrurek@users.noreply.github.com>
2023-05-09 19:29:06 +0200

    update consulatation

--- models/consultation.js
+++ models/consultation.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const consultationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   teacherId: {
     type: Number,
     required: true
